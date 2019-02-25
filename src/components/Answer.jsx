cbd5c5d10ed3ed269684c5670d7f448bcbd56853
otakunerd9 <aisaac8595@gmail.com>
2019-02-25 18:27:06 -0500

    CSS
    It wentttttt throughh changesssssssssssss

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import "../css/Answer.css";
 
 class Answer extends Component {
-  render() {
-    return (
-      <div class="Answer">
-        <button type="button">{this.props.text}</button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="Answer">
+				<button
+					onClick={() => this.props.handleClick(this.props.answerNumber)}
+					type="button"
+				>
+					<font color={this.props.color}>{this.props.text}</font>
+				</button>
+			</div>
+		);
+	}
 }
 
 export default Answer;
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import "../css/Title.css";
 // import components
 
 class Title extends Component {
-  render() {
-    return (
-      <div className="title">
-        <h2>ACTIV</h2>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="title">
+				<h2>ACTIV</h2>
+			</div>
+		);
+	}
 }
 
 export default Title;
